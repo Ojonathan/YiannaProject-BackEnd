aa05c7a623db772cb28761b00df5edaf1ea9eac8
Jonathan <jonathan-08@hotmail.com>
2020-11-15 23:15:58 +0100

    Initial commit

---
+++ src/main/java/be/yianna/YiannaprojectApplication.java
@@ -0,0 +1,13 @@
+package be.yianna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YiannaprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YiannaprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/yianna/YiannaprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package be.yianna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YiannaprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
