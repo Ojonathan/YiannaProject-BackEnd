b8276994e269f10162719d5987f3153f4a966dbf
Jonathan <jonathan-08@hotmail.com>
2020-11-18 23:22:46 +0100

    Database definition and RestControllers empty definitions

---
+++ src/main/java/be/yianna/repository/TypeEventRepository.java
@@ -0,0 +1,4 @@
+package be.yianna.repository;
+
+public interface TypeEventRepository {
+}
---
+++ src/main/java/be/yianna/rest/UserRestController.java
@@ -0,0 +1,9 @@
+package be.yianna.rest;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin // possible to specify settings
+public class UserRestController {
+}
